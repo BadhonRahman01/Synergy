97e3e8ce358e9c87c2b7998637b95c36eea7b66f
Badhon Rahman <badhon.rahman184@gmail.com>
2022-09-06 12:29:25 +0600

    Day4

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Validation\Rules\Password;
-
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -32,14 +32,36 @@ class AuthController extends Controller
 
 
         ]);
-$token = $user->createToken('main')->PlainTextToken;
+        $token = $user->createToken('main')->PlainTextToken;
 
         return response([
                 'user' => $user,
                 'token' => $token
         ]);
 
+    }
+    public function login(Request $request)
+    {
+       $credentials = $request->validate([
+            'email' => 'required|string|exist:user,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean'
+       ]);
+       $remember = $credentials['remember'] ?? false;
+       unset($credentials['remember']);
+       if (!Auth::attempt($credentials, $remember)) {
+        return response([
+            'error' => 'The Provided credentials are not correct'
+        ], 422);
 
-
+       }
+       $user = Auth::user();
+       $token = $user->createToken('main')->plainTextToken;
+       return response([
+        'user' => $user,
+        'token' => $token
+       ]);
     }
 }
